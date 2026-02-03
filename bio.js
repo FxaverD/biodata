0239f31dd9890ee91f18221c0cbc45422e76b680
FxaverD <siskus1602@gmail.com>
2026-02-03 09:56:38 +0700

    birthdate

--- bio.js
+++ bio.js
@@ -3,3 +3,5 @@ var prompt = PromptSync
 var Name = prompt('What is your Name?')
 var age = prompt('How old are you?')
 var status = prompt ('What is your status?')
+
+var birthdate = prompt("Enter your birthdate")
